b2b06a6ccb42c27d448e3d6e020a99514d25ab5e
Kumar Divyanshu <87257292+divyanshuk72@users.noreply.github.com>
2024-11-03 23:05:20 +0530

    Added the current streak logic

--- src/components/habit-list.jsx
+++ src/components/habit-list.jsx
@@ -1,4 +1,11 @@
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  LinearProgress,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -10,6 +17,24 @@ const HabitList = () => {
   const today = new Date().toISOString().split("T")[0];
 
   const dispatch = useDispatch();
+
+  const getStreak = (habit) => {
+    let streak = 0;
+    const currentDate = new Date();
+
+    while (true) {
+      const dateString = currentDate.toISOString().split("T")[0];
+
+      if (habit.completedDates.includes(dateString)) {
+        streak++;
+        currentDate.setDate(currentDate.getDate() - 1);
+      } else {
+        break;
+      }
+    }
+
+    return streak;
+  };
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 4 }}>
@@ -60,6 +85,17 @@ const HabitList = () => {
                 </Box>
               </Grid>
             </Grid>
+            <Box sx={{ mt: 2 }}>
+              <Typography variant="body2">
+                Current Streak : {getStreak(habit)} days
+              </Typography>
+
+              <LinearProgress
+                variant="determinate"
+                value={(getStreak(habit) / 30) * 100}
+                sx={{ mt: 1 }}
+              />
+            </Box>
           </Paper>
         );
       })}
